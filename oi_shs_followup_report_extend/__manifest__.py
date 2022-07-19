102efb1915d0681f38c12d30ed57f009ab7d56fe
Ramya-mahesh <ramya@odooimplementers.com>
2022-07-19 16:58:54 +0530

    update

---
+++ oi_shs_followup_report_extend/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name': 'Follow Up Report - Extended',
+    "version": "12.0.1.0.0",
+    'summary': 'Follow Up Report - Extended',
+    'description': """Customized Followup Report""",
+    'author' : "Itara IT Solutions Pvt Ltd",
+    'website': "www.itarait.com",
+    'category': 'MRP',
+    'depends': ['base', 'account_reports'],
+
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+
+}
