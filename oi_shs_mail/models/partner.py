07da7cd1167f9f6949e123c430bf7a9f1e056307
karthikeyan200499 <karthikeyan.a@odooimplementers.com>
2022-07-22 18:19:48 +0530

    shs_mail cancel

--- oi_shs_mail/models/partner.py
+++ oi_shs_mail/models/partner.py
@@ -168,3 +168,22 @@ class SaleOrderConfirmation(models.Model):
                 'target': 'new',
                 'context': ctx,
             }
+
+
+            
+    def action_cancel(self):
+        cancel_warning = self._show_cancel_wizard()
+        if cancel_warning:
+            return {
+                'name': _('Cancel Sales Order'),
+                'view_mode': 'form',
+                'res_model': 'sale.order.cancel',
+                'view_id': self.env.ref('sale.sale_order_cancel_view_form').id,
+                'type': 'ir.actions.act_window',
+                'context': {'default_order_id': self.id},
+                'target': 'new'
+            }
+        inv = self.invoice_ids.filtered(lambda inv: inv.state == 'draft')
+        inv.button_cancel()
+        self.action_sale_send()
+        return self.write({'state': 'cancel'})
