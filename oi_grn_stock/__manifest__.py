7bfb73e105ccc1763a6dc5b10bf89d7e9c29de85
root <arokiyaliyon@github.com>
2023-05-12 11:48:20 +0530

    update

---
+++ oi_grn_stock/__manifest__.py
@@ -0,0 +1,28 @@
+
+{
+    "name": "Auto Mail",
+    "summary": "Auto mailsssssss",
+    "version": "15.0.1",
+    'category': '',
+    "website": "www.odooimplementers.com",
+	"description": """
+		 	  
+    """,
+	
+    "author": "Oodu Implementers Pvt. Ltd.",
+    "installable": True,
+    "depends": [
+                'base', 'stock', 'sale','product',
+                ],
+    "data": [
+        # 'view/partner_view.xml',
+        # 'view/template.xml',
+        'report/menu_grn_report.xml',
+        'report/grn_report.xml',
+    ],
+    
+    'installable': True,
+    'auto_install': True,    
+       
+}
+
