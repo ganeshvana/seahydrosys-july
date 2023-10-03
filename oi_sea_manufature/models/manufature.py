d4aef8409a1b7c8c5b7caf8929087fd5b6373b25
vidhya <vidhya@odooimplementers.com>
2023-10-03 12:56:44 +0530

    inspection

--- oi_sea_manufature/models/manufature.py
+++ oi_sea_manufature/models/manufature.py
@@ -3,5 +3,5 @@ from odoo import api, fields, models, tools, _
 class MrpProduction(models.Model):
     _inherit = 'mrp.production'
 
-    sea_delivery_date = fields.Date(string="Delivery Date")
-    sea_batch_no = fields.Text(string="Batch No")
+    sea_delivery_date = fields.Date(string="Delivery Date", tracking=True)
+    sea_batch_no = fields.Text(string="Batch No", tracking=True)
