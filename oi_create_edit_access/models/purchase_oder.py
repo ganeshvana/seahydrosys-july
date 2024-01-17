63f84f86a9eb4523bed13621d0a700c932c9bf77
abinayasv1 <abinaya@odooimplementers.com>
2024-01-17 16:19:42 +0530

    Update Tracking

--- oi_create_edit_access/models/purchase_oder.py
+++ oi_create_edit_access/models/purchase_oder.py
@@ -1,6 +1,14 @@
 from odoo import api, fields, models, tools, _
 from odoo.addons.purchase.models.purchase import PurchaseOrder as Purchase
 
+from datetime import datetime, timedelta
+from itertools import groupby
+import json
+
+from odoo import api, fields, models, SUPERUSER_ID, _
+from odoo.exceptions import AccessError, UserError, ValidationError
+from odoo.osv import expression
+from odoo.tools import float_is_zero, html_keep_url, is_html_empty
 
 class PurchaseOrder(models.Model):
     _inherit = "purchase.order"
