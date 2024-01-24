3991eca49eee48870aae6d3632c9d2e175ee88b2
vidhya <vidhya@odooimplementers.com>
2024-01-24 11:01:37 +0530

    Product Tag

--- oi_manufact_add_fields/models/manufact.py
+++ oi_manufact_add_fields/models/manufact.py
@@ -6,7 +6,7 @@ class UserRestriction(models.Model):
 
 
     note = fields.Many2one('mrp.note', string='Note')
-    tag_ids = fields.Many2one(related='product_id.tag_ids', string="Product Tag")
+    # tag_ids = fields.Many2one(related='product_id.tag_ids', string="Product Tag")
     
 
 
