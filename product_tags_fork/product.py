bf6e2803c982c81fb365c68bc82c63436df023f8
vidhya <vidhya@odooimplementers.com>
2024-01-29 10:52:30 +0530

    search

--- product_tags_fork/product.py
+++ product_tags_fork/product.py
@@ -80,9 +80,16 @@ class ProductTemplate(models.Model):
 
 class UserRestriction(models.Model):
     _inherit = "mrp.production"
+    
+    
+    tag_ids = fields.Many2many(string='Tags',
+                               comodel_name='product.tag',
+                               relation='product_product_tag_rel',
+                               column1='tag_id',
+                               column2='product_id')
 
 
-    tag_ids = fields.Many2many( related='product_id.tag_ids', string="Product Tags")
+    # tag_ids = fields.Many2many( related='product_id.tag_ids', string="Product Tags")
     
     # comodel_name='product.tag', relation='mrp_product_tag_rel',
     #                            column1='product_tag_id',
