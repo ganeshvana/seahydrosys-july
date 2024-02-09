4b3955f2fb929665fa94a17e61062dca5a47c365
abinayasv1 <abinaya@odooimplementers.com>
2024-02-09 16:41:43 +0530

    oi_create_edit_access

--- oi_create_edit_access/models/sale_order.py
+++ oi_create_edit_access/models/sale_order.py
@@ -17,61 +17,7 @@ from odoo.addons.payment import utils as payment_utils
 class SaleOrderLine(models.Model):
     _inherit = "sale.order.line"
 
-    # @api.model
-    # def create(self, vals):
-    #     res = super(SaleOrderLine, self).create(vals)
-    #     self._create_notification_log(res, 'created')
-    #     return res
 
-    # def write(self, vals):
-    #     res = super(SaleOrderLine, self).write(vals)
-    #     if vals.get('product_id') or vals.get('name') or vals.get('product_uom_qty'):
-    #         self._create_notification_log(self, 'edited')
-    #     return res
-
-    # def unlink(self):
-    #     deleted_lines = self.filtered(lambda line: line.exists())
-    #     res = super(SaleOrderLine, deleted_lines).unlink()
-    #     for line in deleted_lines:
-    #         self._delete_notification_log(line, 'deleted')
-    #     return res
-    
-    # def _delete_notification_log(self, line, action):
-    #     subtype = self.env['mail.message.subtype'].search([('name', '=', 'Note')], limit=1)
-    #     if action == 'deleted':
-    #         body = '<p>Sale Order Line deleted:</p>'
-
-    #     edit_message = self.env['mail.message'].create({
-    #         'subject': f'{action.capitalize()} in Sale Order Line',
-    #         'body': body,
-    #         'message_type': 'notification',
-    #         'model': 'sale.order',
-    #         'res_id': line.order_id.id,
-    #         'subtype_id': subtype.id
-    #     })
-
-    # def _create_notification_log(self, line, action):
-    #     subtype = self.env['mail.message.subtype'].search([('name', '=', 'Note')], limit=1)
-    #     if action == 'created':
-    #         body = '<p>Sale Order Line created:</p>'
-    #     elif action == 'edited':
-    #         body = '<p>Sale Order Line edited:</p>'
-    #     # elif action == 'deleted':
-    #     #     body = '<p>Sale Order Line deleted:</p>'
-    #     body += f'<p>Product: {line.product_id.name}</p>' if line.product_id else ''
-    #     body += f'<p>Description: {line.name}</p>' if line.name else ''
-    #     body += f'<p>Quantity: {line.product_uom_qty}</p>' if line.product_uom_qty else ''
-
-    #     edit_message = self.env['mail.message'].create({
-    #         'subject': f'{action.capitalize()} in Sale Order Line',
-    #         'body': body,
-    #         'message_type': 'notification',
-    #         'model': 'sale.order',
-    #         'res_id': line.order_id.id,
-    #         'subtype_id': subtype.id
-    #     })
-    
-    
     @api.model
     def create(self, vals):
         res = super(SaleOrderLine, self).create(vals)
