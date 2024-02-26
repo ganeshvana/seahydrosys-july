6aec03473bd5a26cea1e1fbca73bdd8db8cbde48
vidhya <vidhya@odooimplementers.com>
2024-02-26 16:48:41 +0530

    MO

--- oi_sea_manufature/models/manufature.py
+++ oi_sea_manufature/models/manufature.py
@@ -5,3 +5,22 @@ class MrpProduction(models.Model):
 
     sea_delivery_date = fields.Date(string="Delivery Date", tracking=True)
     sea_batch_no = fields.Text(string="Batch No", tracking=True)
+    origin_name = fields.Char(compute='_compute_origin_name',)
+    
+    @api.depends('origin')
+    def _compute_origin_name(self):
+        for mo in self:
+            if mo.origin:
+                # Split values by comma
+                origin_names = mo.origin.split(',')
+                filtered_names = [name.strip() for name in origin_names if name[:2] == mo.name[:2]]
+                
+                if filtered_names:
+                    mo.origin_name = ', '.join(filtered_names)
+                else:
+                    # If mo.origin_name is still empty, try splitting by dash
+                    origin_dash = mo.origin.split('-')
+                    for origin_name in origin_dash:
+                        mo.origin_name = origin_name
+            else:
+                mo.origin_name = False
