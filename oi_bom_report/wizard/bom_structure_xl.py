f829a45f8db8b3c86a45323d5b042c6b75fc9e62
vidhya <vidhya@odooimplementers.com>
2024-02-29 10:52:04 +0530

    BOM

--- oi_bom_report/wizard/bom_structure_xl.py
+++ oi_bom_report/wizard/bom_structure_xl.py
@@ -278,7 +278,8 @@ class MRPBOMStructureXl(models.TransientModel):
 
                 for a in vals['docs']:
                     for line in a['lines']:
-                        if line['type'] != 'operation':
+                        if line['type'] not in ('operation','subcontract'):
+                        # if line['type'] != 'operation':
                             if not 'prod_cost' in line:
                                 line['prod_cost'] = 0.0
                             if not 'bom_cost' in line:
