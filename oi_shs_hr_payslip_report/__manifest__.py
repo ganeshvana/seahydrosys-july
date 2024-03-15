e07d80290e0ac25818c48bdd5fd888271d911010
abinayasv1 <abinaya@odooimplementers.com>
2024-03-15 11:27:49 +0530

    oi_shs_hr_payslip_report

---
+++ oi_shs_hr_payslip_report/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name': 'HR Payslip',
+    "version": "1.0",
+    'summary': '',
+    'author':"Oodu Implementers Private Limited",
+    'description': """This module fetches data from Payslip in PDF format""",
+    'category': 'HR Payslip',
+    'depends': ['base', 'stock',],
+    'data': [
+            # 'views/inspection_report_view.xml',
+            # 'views/inspection_template_view.xml'
+            ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+
+}
