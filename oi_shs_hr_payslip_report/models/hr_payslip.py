33dddc1d4937c32db622a4080e7648836bc078cd
abinayasv1 <abinaya@odooimplementers.com>
2024-03-15 13:13:07 +0530

    oi_shs_hr_payslip_report

---
+++ oi_shs_hr_payslip_report/models/hr_payslip.py
@@ -0,0 +1,9 @@
+from odoo import models, fields, api, _
+
+class StylAccount(models.Model):
+    _inherit = "hr.payslip"
+    
+    def print_payslip_report(self)    :
+        return self.env.ref('oi_shs_hr_payslip_report.action_print_report_hr_payslip').report_action(self)
+
+
