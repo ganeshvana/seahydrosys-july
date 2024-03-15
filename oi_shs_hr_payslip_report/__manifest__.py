d36af9bcdc4f8e46c0844357c86f1b863cc984fd
abinayasv1 <abinaya@odooimplementers.com>
2024-03-15 12:19:58 +0530

    oi_shs_hr_payslip_report

--- oi_shs_hr_payslip_report/__manifest__.py
+++ oi_shs_hr_payslip_report/__manifest__.py
@@ -7,7 +7,7 @@
     'author':"Oodu Implementers Private Limited",
     'description': """This module fetches data from Payslip in PDF format""",
     'category': 'HR Payslip',
-    'depends': ['base','hr','hr_payroll',],
+    'depends': ['base','hr','hr_payroll','hr_contract'],
     'data': [
             'views/payslip_report_view.xml',
             'views/payslip_template_view.xml'
