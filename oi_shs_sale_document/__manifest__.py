fd2b5e1896617a32226c53673826d94831e70514
Rohini260401 <rohini@odooimplementers.com>
2024-05-23 10:30:02 +0530

    update

--- oi_shs_sale_document/__manifest__.py
+++ oi_shs_sale_document/__manifest__.py
@@ -7,7 +7,7 @@
     'author': 'Oodu Implementers Pvt. Ltd',
     'description': """This module contains extended view of sale document""",
     'category': 'Sales',
-    'depends': ['base', 'sale', 'sale_stock],
+    'depends': ['base', 'sale', 'sale_stock'],
     'data': [
     'views/sale_view.xml',
     'views/sale_report_view.xml',
