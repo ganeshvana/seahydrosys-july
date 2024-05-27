13ec27061aeac300eaad450c2f2b47161a310aa5
vidhya <vidhya@odooimplementers.com>
2024-05-27 17:45:06 +0530

    update

--- oi_shs_sale_document/models/sale.py
+++ oi_shs_sale_document/models/sale.py
@@ -7,11 +7,4 @@ class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
     customer_delivery_date = fields.Date("Shipping Date")
-<<<<<<< HEAD
-    quatation_order_date = fields.Date(string="Order Date")
-    # shipping_date = fields.Date(string="Shipping Date")
-=======
->>>>>>> 10b7c9b828f7e3e9571c03f290ffe922a9de1cd6
-
-
 
