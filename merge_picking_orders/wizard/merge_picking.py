06f91528c758277c80c674de5fcb101e6f9a1025
vigneshtm94 <tmvignesh1994@gmail.com>
2024-07-02 12:44:50 +0530

    Updates

--- merge_picking_orders/wizard/merge_picking.py
+++ merge_picking_orders/wizard/merge_picking.py
@@ -89,7 +89,9 @@ class MergePicking(models.TransientModel):
         for record in orders:
             for line in record.move_lines:
                 moves += line.copy({'picking_id': main_pick.id,
-                                    'description': f"{record.customer_reference or ''} {record.customer_reference or ''}"})
+                                    'description' : record.customer_reference
+                                    # 'description': f"{line.description or ''} {record.customer_reference or ''}"
+                                    })
             source_document.append(record.name + ' - ' + record.origin)
             reference.append(record.customer_reference if record.customer_reference else '')
             if record.batch_id:
