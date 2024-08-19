302bb4a5b09be944a3e44fa105372101283a5776
vidhya <vidhya@odooimplementers.com>
2024-08-19 17:17:35 +0530

    Report changes

--- oi_shs_sea_export_inv/__manifest__.py
+++ oi_shs_sea_export_inv/__manifest__.py
@@ -7,10 +7,11 @@
     'author':"Oodu Implementers Pvt. Ltd.",
     'description': """Export Invoice Document""",
     'category': 'Accounts',
-    'depends': ['base', 'account', 'oi_shs_exchange_rate'],
+    'depends': ['base', 'account', 'oi_shs_exchange_rate','stock_account'],
     'data': [
             'views/export_invoice_report_view.xml',
-            'views/export_invoice_template_view.xml'
+            'views/export_invoice_template_view.xml',
+            'views/invoice_report.xml'
             ],
     'installable': True,
     'application': True,
