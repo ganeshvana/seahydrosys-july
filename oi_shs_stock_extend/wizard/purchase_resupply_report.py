5422232aca3cd908ad71ea820d3967f377c24238
kowsalyaG3024 <kowsalya@odooimplementers.com>
2024-08-30 16:10:06 +0530

    updates new

--- oi_shs_stock_extend/wizard/purchase_resupply_report.py
+++ oi_shs_stock_extend/wizard/purchase_resupply_report.py
@@ -108,17 +108,16 @@ class ResupplyReport(models.TransientModel):
 
                             # Write 'Receipt No' (picking name)
                             worksheet.write(row, col, str(move.picking_id.name), style_normal)
-                            # col += 1
+                            col += 1
                             
                             # Write 'Supply No' and 'Supply Date'
                             pick = pickings.filtered(lambda m: m.product_id == pol.product_id)
                             for val in pick:
-                                col = 6
                                 worksheet.write(row, col, str(val.picking_id.name), style_normal)
                                 col += 1
                                 
                                 worksheet.write(row, col, str(val.picking_id.date_done or ''), style_normal)
-                            # col += 1
+                            col += 1
                             if val.picking_id.state == 'draft':
                                 state = 'Draft'
                             if val.picking_id.state == 'waiting':
@@ -131,7 +130,7 @@ class ResupplyReport(models.TransientModel):
                                 state = 'Done'
                             if val.picking_id.state == 'cancel':
                                 state = 'Cancel'
-                            # col=
+                            
                             worksheet.write(row, col, str(state),style_normal)
                             col += 1
                             pick_sum = sum(p.quantity_done for p in pick)
