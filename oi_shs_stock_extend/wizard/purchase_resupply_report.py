7c2795d405506ad8a35f5ed8768afbaa5023122c
arokiyaliyon <arokiyaliyon@odooimplementers.com>
2024-09-05 12:43:39 +0530

    update

--- oi_shs_stock_extend/wizard/purchase_resupply_report.py
+++ oi_shs_stock_extend/wizard/purchase_resupply_report.py
@@ -71,7 +71,7 @@ class ResupplyReport(models.TransientModel):
                                 worksheet.write(row, col, val.picking_id.date_done.strftime('%d/%m/%Y') if val.picking_id.date_done else '', style_normal)
                                 col += 1
                                 # .........
-                                worksheet.write(row,  col, val.picking_id.customer_reference, style_normal)
+                                worksheet.write(row,  col,  str(val.picking_id.customer_reference), style_normal)
                                 col += 1
                                 # .........
 
