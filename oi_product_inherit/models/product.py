bfa8cf4dffb21fb2f508c251dc6888bfed6eabed
Rohini260401 <rohini@odooimplementers.com>
2024-10-13 18:46:23 +0530

    [FIX] product  seq

--- oi_product_inherit/models/product.py
+++ oi_product_inherit/models/product.py
@@ -6,8 +6,6 @@ import re
 
 
 
-
-
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
 
@@ -16,22 +14,21 @@ class ProductTemplate(models.Model):
     @api.model
     def create(self, vals):
         if vals.get('categ_id'):
-            category = self.env['product.category'].browse(vals['categ_id'])
-            vals['product_reference'] = self._generate_sequence(category)
+            new_category = self.env['product.category'].browse(vals['categ_id'])
+            product_code_new = new_category.product_code or new_category.name[:3].upper()
+            vals['product_reference'] = self._generate_sequence(new_category)
+
         return super(ProductTemplate, self).create(vals)
 
     def write(self, vals):
         if vals.get('categ_id'):
             new_category = self.env['product.category'].browse(vals['categ_id'])
             product_code_new = new_category.product_code or new_category.name[:3].upper()
-
             for product in self:
                 current_category = product.categ_id
                 product_code_current = current_category.product_code or current_category.name[:3].upper()
                 if product_code_new != product_code_current:
                     vals['product_reference'] = self._generate_sequence(new_category)
-                else:
-                    vals['product_reference'] = product.product_reference
         return super(ProductTemplate, self).write(vals)
 
     def _generate_sequence(self, category):
@@ -41,13 +38,13 @@ class ProductTemplate(models.Model):
             last_product = self.env['product.template'].search(domain, order="id desc", limit=1)
             next_sequence_number = 1
             if last_product and last_product.product_reference:
-                match = re.search(rf'{prefix}(\d+)', last_product.product_reference)
+                match = re.search(rf'{re.escape(prefix)}(\d+)', last_product.product_reference)
                 if match:
                     last_sequence_number = int(match.group(1))
                     next_sequence_number = last_sequence_number + 1
             next_reference = f"{prefix}{str(next_sequence_number).zfill(5)}"
             return next_reference
-        
+
         return False
 
     @api.constrains('purchase_ok', 'seller_ids')
