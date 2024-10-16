47201b6e7ab035093fa7573b7c236a608124e62f
GVigneshwara <vigneshgopi1567@gmail.com>
2024-10-16 13:20:17 +0530

    updates

--- oi_sea_manufature/models/manufature.py
+++ oi_sea_manufature/models/manufature.py
@@ -1,7 +1,18 @@
 from odoo import api, fields, models, tools, _
+from odoo.exceptions import ValidationError
+
 
 class MrpProduction(models.Model):
     _inherit = 'mrp.production'
 
     sea_delivery_date = fields.Date(string="Delivery Date", tracking=True)
     sea_batch_no = fields.Text(string="Batch No", tracking=True)
+    
+    @api.multi
+    def button_mark_done(self):
+        for production in self:
+            pickings = production.picking_ids.filtered(lambda p: p.state != 'done')
+            if pickings:
+                raise ValidationError("You cannot validate the Manufacturing Order until all related stock transfers are in 'Done' state.")
+            # Call the super method to continue with validation
+            return super(MrpProduction, self).button_validate()
